7f9d8f87a096949caf8f57f565c0a25ea2414fc5
Irem Nur Gozudok <iremgozudok99@icloud.com>
2023-05-22 16:49:50 +0300

    timer componenti olusturuldu

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Timer from "./components/Timer";
+
+function App() {
+  return (
+    <>
+      <main>
+        <Timer />
+      </main>
+    </>
+  );
+}
+
+export default App;
